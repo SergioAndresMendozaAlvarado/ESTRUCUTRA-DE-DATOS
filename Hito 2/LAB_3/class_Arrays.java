45dc58a8f02ce5e69d57c3aca2a9cc69f42dbca4
SergioAndresMendozaAlvarado <116565121+SergioAndresMendozaAlvarado@users.noreply.github.com>
2023-03-16 14:16:44 +0300

    Create class_Arrays.java

---
+++ Hito 2/LAB_3/class_Arrays.java
@@ -0,0 +1,24 @@
+public class Arrays {
+//tenemos doS arrays con los siguientes valores:
+    //ARRAY_1 = [33,20,19,42,25]
+    //ARRAY_2 = [30,20,19,13,25]
+    //SE DEBE REALIZAR EL CAMBIO DE POSICION DEL ARRAY 1 POSICION 3 A LA POSICION 0 DEL ARRAY 2
+    //Y A LA VEZ EL VALOR DE LA POSICION 0 DEL ARRAY 2 DEBE CAMBIAR A LA POSICION 3 DEL ARRAY 1
+    //USANDO POO
+
+    //APLICANDO PRUEBA Y ERROR:
+
+    private int[] Arrays;
+    public Arrays(int[] Arrays){
+
+    }
+    public int[] getArrays() {
+        return Arrays;
+    }
+
+    public void setArrays(int[] arrays) {
+        Arrays = arrays;
+    }
+
+    
+}
