f076d0d484c6bfd99187ce9967946510cfd6ee9d
SergioAndresMendozaAlvarado <116565121+SergioAndresMendozaAlvarado@users.noreply.github.com>
2023-04-13 14:53:48 +0100

    Create Main_cadenas.java

---
+++ Hito_3/Main_cadenas.java
@@ -0,0 +1,16 @@
+package PILAS;
+
+public class Main_cadenas{
+    public static void main(String[] args)
+    {
+        PilaDeCadenas pilaC = new PilaDeCadenas();
+        pilaC.Adicionar("William");
+        pilaC.Adicionar("Andres");
+        pilaC.Adicionar("Josias");
+        pilaC.Adicionar("Iris");
+        pilaC.Adicionar("Jonathan");
+        pilaC.Adicionar("Ilia");
+        pilaC.Mostrar();
+
+    }
+}
