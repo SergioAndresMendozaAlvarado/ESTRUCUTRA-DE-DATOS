6449d926e62010742790073d184887f9db0a2537
SergioAndresMendozaAlvarado <116565121+SergioAndresMendozaAlvarado@users.noreply.github.com>
2023-05-05 06:43:06 +0300

    Create Cliente(class).java

---
+++ Hito_3_Evaluacion/El Main, pilas , clase/Cliente(class).java
@@ -0,0 +1,69 @@
+package Hito3_eval;
+
+public class Cliente {
+
+    private String nombres;
+    private String apellidos;
+    private int edad;
+    private String direccion;
+    private String genero;
+
+    public Cliente(String nombres, String apellidos, int edad, String direccion, String genero) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.genero = genero;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public void motrarCliente(){
+
+        System.out.println("\nMostrando datos del Cliente");
+        System.out.println("Nombre: "+this.nombres);
+        System.out.println("Apellidos: "+this.apellidos);
+        System.out.println("Edad: "+this.edad);
+        System.out.println("Direccion: "+this.direccion);
+        System.out.println("Genero: "+this.genero);
+
+    }
+}
